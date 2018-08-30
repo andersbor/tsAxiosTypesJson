3096cbfb6fe29f7505f06d9b8dc1f726a9307002
andersbor <anbo@easj.dk>
2018-08-30 13:19:57 +0200

    collapsible list added

--- src/js/index.ts
+++ src/js/index.ts
@@ -21,7 +21,7 @@ interface IComment {
     body: string;
 }
 
-// more advanced object
+// more advanced object, including nested objects (and double nested objects)
 interface IUser {
     id: number;
     name: string;
@@ -60,7 +60,7 @@ axios.get<IComment>("http://jsonplaceholder.typicode.com/comments").
 */
 
 axios.get<IUser>("http://jsonplaceholder.typicode.com/users").
-then(function (response: AxiosResponse<IComment[]>): void {
+then(function (response: AxiosResponse<IUser[]>): void {
    // buildAndShowLongHtmlString(response);
    addToDom(response);
 })
@@ -73,24 +73,33 @@ function buildAndShowLongHtmlString(response: AxiosResponse<IComment[]>): void {
     let result: string = "<ol>";
     response.data.forEach((user: IUser) => {
         console.log(user.email);
-        result += "<li>" + user.name + " " + user.email + " " + user.address.city + "</li>";
+        result += "<li>" + user.username + " " + user.email + " " + user.phone + "</li>";
     });
     result += "</ol>";
     outputElement.innerHTML = result;
 }
 
-function addToDom(response: AxiosResponse<IComment[]>): void {
+function addToDom(response: AxiosResponse<IUser[]>): void {
     response.data.forEach((user: IUser) => {
         let buttonElement: HTMLButtonElement = document.createElement<"button">("button");
         buttonElement.innerHTML = user.id + " " + user.name;
+        buttonElement.className = "collapsible";
         outputElement.appendChild(buttonElement);
 
         let divElement : HTMLDivElement = document.createElement<"div">("div");
-        divElement.innerHTML = user.name + " " + user.email + " " + user.address.city;
+        divElement.innerHTML = user.email + " " + user.address.city + " " +  user.company.name;
+        divElement.className = "content";
+        divElement.style.display = "none";
         outputElement.appendChild(divElement);
 
         buttonElement.addEventListener("click", ()=> {
             console.log("clicked " + user.name);
-        })
+            console.log(divElement.style.display);
+            if (divElement.style.display === "block") {
+                divElement.style.display = "none";
+            } else {
+                divElement.style.display = "block";
+            }
+        });
     });
 }
